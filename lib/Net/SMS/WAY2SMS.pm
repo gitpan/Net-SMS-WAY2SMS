6bb449c168aa8119b87234f8c6bdbadf4afc3ad0
ADARSH TP <adarshtp@cpan.org>
2014-09-23 16:07:32 +0000

    Import of ADARSHTP/Net-SMS-WAY2SMS-0.10 from CPAN.
    gitpan-cpan-distribution: Net-SMS-WAY2SMS
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         ADARSHTP/Net-SMS-WAY2SMS-0.10.tar.gz
    gitpan-cpan-author:       ADARSHTP
    gitpan-cpan-maturity:     released

--- lib/Net/SMS/WAY2SMS.pm
+++ lib/Net/SMS/WAY2SMS.pm
@@ -8,7 +8,7 @@ use warnings;
 use WWW::Mechanize;
 use Compress::Zlib;
 use Carp qw( croak );
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 __END__
 
@@ -18,7 +18,7 @@ Net::SMS::WAY2SMS - Send SMS to any mobile phones in India using way2sms.com
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
